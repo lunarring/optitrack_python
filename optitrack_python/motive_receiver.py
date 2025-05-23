2f7369f75ca904ba6a6031b17a88f24e27423c5d
PedrCorreia <correia.18.pedro@gmail.com>
2025-05-23 13:06:40 +0100

    high level copied from motion_comfy

---
+++ optitrack_python/motive_receiver.py
@@ -0,0 +1,229 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import os
+import numpy as np
+import math
+import time
+import threading
+import sys
+# # from icecream import ic
+
+# #from util import euler_from_quaternion
+# sys.path.append("/home/lugo/git/NatNetSDK_4.1.0")
+
+from optitrack_python.streaming.NatNetClient import NatNetClient
+
+
+def compute_sq_distances(a, b):
+    # Calculate differences using broadcasting
+    diff = a[:, np.newaxis, :] - b[np.newaxis, :, :]
+    # Calculate squared Euclidean distances
+    dist_squared = np.sum(diff ** 2, axis=2)
+    ## Take square root to get Euclidean distances
+    # distances = np.sqrt(dist_squared)
+    
+    # Create dictionary to store dist_squared with index pairs
+    distance_dict = {(i_a, i_b): dist_squared[i_a, i_b] for i_a in range(dist_squared.shape[0]) for i_b in range(dist_squared.shape[1])}
+    distance_dict = dict(sorted(distance_dict.items(), key=lambda item: item[1]))
+    return distance_dict
+
+
+class MotiveReceiver:
+    def __init__(self, server_ip, client_ip, max_buffer_size=100000, start_process=True, do_record_streaming=False, do_mock_streaming=False, fn_mock='streaming_mock.pkl'):
+        self.server_ip = server_ip
+        self.client_ip = client_ip
+        self.max_buffer_size = max_buffer_size
+        self.marker_data = []
+        self.lock = threading.Lock()
+        self.running = True
+        self.sleep_time = 0.000001
+        self.list_raw_packets = []
+        self.list_dict_packets = []
+        
+        
+        self.v_last_time = 0
+        self.v_sampling_time = 0.01
+        self.last_frame_id = 0
+        self.list_labels = []
+        self.dict_label_idx = {}
+        self.set_labels = set()
+        self.list_unlabeled = []
+        self.list_timestamps = []
+        
+        self.max_nr_markers = 999
+        self.positions = np.zeros([self.max_buffer_size, self.max_nr_markers, 3])*np.nan
+        self.velocities = np.zeros([self.max_buffer_size, self.max_nr_markers, 3])
+        self.pos_idx = 0
+        self.last_timestamp = None
+                        
+        self.do_record_streaming = do_record_streaming
+        self.do_mock_streaming = do_mock_streaming
+        self.fn_mock = fn_mock
+        # self.rigid_body_positions = {label:[] for label in self.rigid_body_labels}
+        if start_process:
+            self.start_process()
+
+    def get_last(self,simbly,model_name=""):
+        if model_name in self.list_dict_packets[-1][simbly].keys():
+            return self.list_dict_packets[-1][simbly][model_name]
+        else:
+            return self.list_dict_packets[-1][simbly]
+
+    def get_last_timestamp(self):
+        if len(self.list_dict_packets) > 0:
+            return self.list_dict_packets[-1]['timestamp']
+        else:
+            return 0
+                
+
+    def start_process(self):
+        self.thread = threading.Thread(target=self.get_data)
+        self.thread.start()
+
+    def get_data(self):
+        optionsDict = {}
+        optionsDict["clientAddress"] = self.client_ip
+        optionsDict["serverAddress"] = self.server_ip
+        optionsDict["use_multicast"] = True
+
+        self.streaming_client = NatNetClient()
+        if self.do_record_streaming:
+            self.streaming_client.set_record_streaming(fn_mock=self.fn_mock)
+        if self.do_mock_streaming:
+            self.streaming_client.set_mock_streaming(fn_mock=self.fn_mock)
+        self.streaming_client.set_client_address(optionsDict["clientAddress"])
+        self.streaming_client.set_server_address(optionsDict["serverAddress"])
+        self.streaming_client.set_use_multicast(optionsDict["use_multicast"])
+        self.streaming_client.new_frame_listener = self.process_packet
+            
+        is_running = self.streaming_client.run()
+        if not is_running:
+            print("ERROR: Could not start streaming client.")
+            try:
+                sys.exit(1)
+            except SystemExit:
+                print("...")
+            finally:
+                print("exiting")
+            
+
+    def save_packet(self, packet_content):
+        self.list_raw_packets.append(packet_content)
+        
+    def normalizer_data(self, dict_data):
+        labels_rows = [_["model_name"] for _ in dict_data["marker_sets_labeled_data"]]
+        dict_data["rigid_bodies_full"] = {}
+
+        for __ in dict_data["rigid_bodies"]:
+            id_ = __["id_num"]-1
+            out = __
+            out["model_name"] = labels_rows[id_]
+            out["markers"] = dict_data["marker_sets_labeled_data"][id_]["marker_pos_list"]
+            if len(dict_data["labeled_markers"])-1 >= id_ and id_ >= 0:
+                #print("id",id_)
+                #print(dict_data["labeled_markers"])
+                out["labeled_markers"] = dict_data["labeled_markers"][id_]
+            model_name = out["model_name"].decode()
+            del out["model_name"]
+            dict_data["rigid_bodies_full"][model_name] = out
+        return dict_data
+    
+    def process_packet(self, data_dict):
+        #print(data_dict["mocap_data"].skeleton_data.__dict__)
+        if self.do_mock_streaming:
+            dict_data = {
+                "frame_id": data_dict["frame_number"],
+                "timestamp" : data_dict["mocap_data"].suffix_data.timestamp,
+                "marker_sets_unlabeled_data" : data_dict["mocap_data"].marker_set_data.unlabeled_markers.__dict__,
+                "unlabeled_markers" : data_dict["mocap_data"].legacy_other_markers.__dict__,
+                "marker_sets_labeled_data" : [_.__dict__ for _ in data_dict["mocap_data"].marker_set_data.marker_data_list],
+                "unlabeled_markers" : data_dict["mocap_data"].legacy_other_markers.__dict__,
+                "labeled_markers" : [_.__dict__ for _ in data_dict["mocap_data"].labeled_marker_data.labeled_marker_list],
+                "rigid_bodies" : [_.__dict__ for _ in data_dict["mocap_data"].rigid_body_data.rigid_body_list],
+                # "asset_data" : data_dict["mocap_data"].asset_data.__dict__,
+                "asset_data" : None,
+                "skeletons_list" : [_.__dict__ for _ in data_dict["mocap_data"].skeleton_data.skeleton_list]
+            }
+        else:
+            dict_data = {
+                "frame_id": data_dict["frame_number"],
+                "timestamp" : data_dict["mocap_data"].suffix_data.timestamp,
+                "marker_sets_unlabeled_data" : data_dict["mocap_data"].marker_set_data.unlabeled_markers.__dict__,
+                "unlabeled_markers" : data_dict["mocap_data"].legacy_other_markers.__dict__,
+                "marker_sets_labeled_data" : [_.__dict__ for _ in data_dict["mocap_data"].marker_set_data.marker_data_list],
+                "unlabeled_markers" : data_dict["mocap_data"].legacy_other_markers.__dict__,
+                "labeled_markers" : [_.__dict__ for _ in data_dict["mocap_data"].labeled_marker_data.labeled_marker_list],
+                "rigid_bodies" : [_.__dict__ for _ in data_dict["mocap_data"].rigid_body_data.rigid_body_list],
+                "asset_data" : data_dict["mocap_data"].asset_data.__dict__,
+                "skeletons_list" : [_.__dict__ for _ in data_dict["mocap_data"].skeleton_data.skeleton_list]
+            }
+            
+        #if len(dict_data["skeletons_list"]) > 0:
+        for ii,_ in enumerate(dict_data["skeletons_list"]):
+            if "rigid_body_list" in _.keys():
+                for i,__ in enumerate(_["rigid_body_list"]):
+                    dict_data["skeletons_list"][ii]["rigid_body_list"][i] = __.__dict__
+        #print(dict_data["skeletons_list"])
+        dict_data = self.normalizer_data(dict_data)
+        self.list_dict_packets.append(dict_data)
+        
+
+    def stop(self):
+        print("stopping process!")
+        self.running = False
+        self.process.kill()
+        self.process.wait()
+        self.thread.join()
+        
+    def get_last(self, label=None):
+        if len(self.list_dict_packets) == 0:
+            return None
+        else:
+            if label is None:
+                return self.list_dict_packets[-1]
+            else:
+                if label in self.list_dict_packets[-1].keys():
+                    return self.list_dict_packets[-1][label]
+                else:
+                    return None
+                
+
+    
+
+
+# if __name__ == "__main__body":
+#     motive = MotiveReceiver("10.40.48.84", "10.40.50.40", do_record_streaming=True, do_mock_streaming=False, fn_mock='skeleton_mock.pkl', start_process = True)
+    
+#     self = BodyTracker(motive=motive)
+    
+#     time.sleep(0.05)
+#     while True:
+#         # right_hand.update()
+#         # print(motive.get_last())
+#         self.update()
+#         # if len(self._head.positions) > 1:
+#         #     print(f"LH {self.positions_left_hand[-1]} RH {self.positions_right_hand[-1]}")
+            
+
+
+# if __name__ == "__main__":
+#     motive = MotiveReceiver("10.40.48.84", '10.40.50.40')
+    
+#     self = RigidBody(motive, "A")
+    
+#     while True:
+#         self.update()
+#         # print(self.fract_xz[0])
+        
+#         velocity_norm = np.linalg.norm(self.v_last)
+#         acceleration_norm = np.linalg.norm(self.a_last)
+        
+#         print(f'Velocity norm: {velocity_norm:.5f}' if velocity_norm is not None else '', 
+#               f'Acceleration norm: {acceleration_norm:.5f}' if acceleration_norm is not None else '')
+
+#         # if len(self.positions) > 0:
+#         #     print(f'self.positions {self.positions[-1]}')
+        
+
+#         time.sleep(0.1)
+        
---
+++ optitrack_python/rigid_body.py
@@ -0,0 +1,203 @@
+import os
+import numpy as np
+import math
+import time
+import threading
+import sys
+import lunar_tools as lt
+
+class RigidBody:
+    def __init__(self, motive_receiver, label):
+        self.motive_receiver = motive_receiver
+        self.label = label
+        self.buffer_size = 100
+        self.dt = lt.SimpleNumberBuffer(buffer_size=self.buffer_size)
+        self.positions = lt.NumpyArrayBuffer(buffer_size=self.buffer_size, default_return_value=np.zeros(3))
+        self.velocities = lt.NumpyArrayBuffer(buffer_size=self.buffer_size, default_return_value=np.zeros(3))
+        # self.accelerations = lt.NumpyArrayBuffer(buffer_size=self.buffer_size, default_return_value=np.zeros(3))
+        # self.jerks = lt.NumpyArrayBuffer(buffer_size=self.buffer_size, default_return_value=np.zeros(3))
+        self.angular_velocities = lt.NumpyArrayBuffer(buffer_size=self.buffer_size, default_return_value=np.zeros(3))
+        self.orientations = lt.NumpyArrayBuffer(buffer_size=self.buffer_size, default_return_value=np.zeros(4))
+        self.euler_angles = lt.NumpyArrayBuffer(buffer_size=self.buffer_size, default_return_value=np.zeros(3))
+
+        self.forces = []
+        self.buffer_size = 1000
+
+        self.corners_x = [3.1, -3.5]
+        self.corners_y = [0, 2]
+        self.corners_z = [-2.1, 3.1]
+        self.fract_xz = np.zeros(2)
+        self.fract_xyz = np.zeros(3)
+        self.t_last = 0
+        self.t_current = 0
+        
+        # self.v_current = np.array([0, 0, 0])
+        # self.v_last = np.array([0, 0, 0])
+        # self.a_current = [0, 0, 0]
+        # self.a_last = [0, 0, 0]
+
+
+    def update(self):
+        rigid_bodies_data = self.motive_receiver.get_last("rigid_bodies_full")
+        if rigid_bodies_data is None:
+            return
+        if self.label not in rigid_bodies_data:
+            return
+        body_data = rigid_bodies_data[self.label]
+        self.t_last = self.t_current
+        self.t_current = self.motive_receiver.get_last_timestamp()
+        dt = self.t_current - self.t_last
+        if dt == 0:
+            # print("WARNING! dt between packages is zero!! returning...")
+            # there is nothing to do. return!
+            return
+        self.dt.append(dt)
+        position = np.array(body_data["pos"])
+        self.positions.append(position)
+
+        orientation = np.array(body_data["rot"])
+        self.orientations.append(orientation)
+        self.euler_angles.append(euler_from_quaternion(*orientation))
+
+        if len(self.euler_angles.buffer) >= 2:
+
+            # angular_velocity = self.euler_angles.get_last()[-1] - self.euler_angles.get_last()[-2]
+
+            angular_velocity = self.euler_angles.buffer[-1] - self.euler_angles.buffer[-2]
+
+            self.angular_velocities.append(angular_velocity)
+
+        if len(self.positions.buffer) <= 2:
+            return
+        
+        if len(self.positions.buffer) >= 2:
+            v_current = (self.positions.buffer[-1] - self.positions.buffer[-2]) / dt
+            self.velocities.append(v_current)
+
+        self.get_xz_fract()
+        self.get_xyz_fract()
+
+
+    def get_xz_fract(self):
+        if len(self.positions.buffer) > 1:
+            current_x = self.positions.get_last()[0]
+            current_z = self.positions.get_last()[2]
+
+            self.fract_xz[0] = get_fract(self.corners_x[0], self.corners_x[1], current_x)
+            self.fract_xz[1] = get_fract(self.corners_z[0], self.corners_z[1], current_z)
+
+    def get_xyz_fract(self):
+        if len(self.positions.buffer) > 1:
+            current_x = self.positions.get_last()[0]
+            current_y = self.positions.get_last()[1]
+            current_z = self.positions.get_last()[2]
+            
+            # print(f'current_y {current_y}')
+
+            self.fract_xyz[0] = get_fract(self.corners_x[0], self.corners_x[1], current_x)
+            self.fract_xyz[1] = get_fract(self.corners_y[0], self.corners_y[1], current_y)
+            self.fract_xyz[2] = get_fract(self.corners_z[0], self.corners_z[1], current_z)
+
+
+
+
+
+class BodyTracker:
+    """
+    This class provides a minimal way to get relative hand positions to the heart center.
+    It uses the Motive software"s tracking data to calculate the positions of the hands relative to the heart center.
+    The positions are updated in real-time as the Motive software tracks the movement of the hands and the heart center.
+    """
+    def __init__(self, motive):
+        self.motive = motive
+        self._head = RigidBody(self.motive, "C")
+        self._left_hand = RigidBody(self.motive, "A")
+        self._right_hand = RigidBody(self.motive, "B")
+        self.offset_heart = np.array([0, -0.45, 0])
+        self.positions_heart = []
+        self.positions_left_hand = []
+        self.positions_right_hand = []
+        
+
+    def update(self):
+        # First update all raw rigid bodies
+        self._head.update()
+        self._left_hand.update()
+        self._right_hand.update()
+        
+        # Then compute heart center
+        if len(self._head.positions) > 1:
+            self.positions_heart.append(self._head.positions[-1] + self.offset_heart)
+        
+        # Then get relative positions for the left and right hand
+        if len(self._left_hand.positions) > 1:
+            lh = self._left_hand.positions[-1] - self.positions_heart[-1]
+            lh[0] = -lh[0]
+            self.positions_left_hand.append(lh)
+        if len(self._right_hand.positions) > 1:
+            rh = self._right_hand.positions[-1] - self.positions_heart[-1]
+            rh[0] = -rh[0]
+            self.positions_right_hand.append(rh)
+            
+            # self.positions_heart.append(self._head.positions[-1] + self.offset_heart)
+
+
+def euler_from_quaternion(x, y, z, w):
+    """
+    Convert a quaternion into euler angles (roll, pitch, yaw)
+    roll is rotation around x in radians (counterclockwise)
+    pitch is rotation around y in radians (counterclockwise)
+    yaw is rotation around z in radians (counterclockwise)
+    """
+    t0 = +2.0 * (w * x + y * z)
+    t1 = +1.0 - 2.0 * (x * x + y * y)
+    roll_x = math.atan2(t0, t1)
+ 
+    t2 = +2.0 * (w * y - z * x)
+    t2 = +1.0 if t2 > +1.0 else t2
+    t2 = -1.0 if t2 < -1.0 else t2
+    pitch_y = math.asin(t2)
+ 
+    t3 = +2.0 * (w * z + x * y)
+    t4 = +1.0 - 2.0 * (y * y + z * z)
+    yaw_z = math.atan2(t3, t4)
+    
+    euler_angles = np.array([roll_x, pitch_y, yaw_z])
+ 
+    return euler_angles # in radians
+
+def get_fract(val_fract0, val_fract1, val):
+    do_swap = False
+    # Check if val_fract0 is larger than val_fract1
+    if val_fract0 > val_fract1:
+        do_swap = True
+        val_fract0, val_fract1 = val_fract1, val_fract0
+
+    # Check if val is within the range of val_fract0 and val_fract1
+    if val_fract0 <= val <= val_fract1:
+        # Calculate the fraction of the distance val is between val_fract0 and val_fract1
+        fract = (val - val_fract0) / (val_fract1 - val_fract0)
+    else:
+        # If val is outside the range, return 0 or 1 depending on which side it's closer to
+        fract = 0 if val < val_fract0 else 1
+
+    if do_swap:
+        fract = 1-fract
+    return fract
+
+
+if __name__ == "__main__":
+    from optitrack_python.motive_receiver import MotiveReceiver
+    motive = MotiveReceiver(server_ip="10.40.48.84", client_ip="10.40.50.40")
+    
+    self = BodyTracker(motive=motive)
+    
+    time.sleep(0.05)
+    while True:
+        time.sleep(0.05)
+        print(self._head.positions.get_last())
+        self._head.update()
+        
+        # right_hand.update()
+        # print(motive.get_last())
+        # self.update()
